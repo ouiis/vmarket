fbe7804d57d596b8a78817ecb40a73d5b7469643
ouiis <guirilchen@gmail.com>
2018-11-23 14:40:25 +0800

    dist

--- src/filters/date.js
+++
@@ -1,8 +0,0 @@
-export default function (timestamp) {
-  let time = new Date(timestamp * 1000);
-  let yyyy = time.getFullYear();
-  let mm = ("0" + (time.getMonth() + 1)).substr(-2);
-  let dd = ("0" + time.getDate()).substr(-2);
-
-  return `${yyyy}-${mm}-${dd}`;
-}
